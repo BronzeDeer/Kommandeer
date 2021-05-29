4945b8c34dd687dd85835e9e12d1957d6ee1b6ce
Joel Pepper <joel.pepper@rwth-aachen.de>
2021-05-29 17:00:32 +0200

    Added first proper commandline flag for namespace
    For Kommandeer to take on more features in the future we will need a
    proper cli. Added getopts loop for easier expansion in the future

--- build/kommandeer.sh
+++ build/kommandeer.sh
@@ -1,9 +1,41 @@
-#! /bin/bash -e
+#! /bin/bash
+
+unset NAMESPACE
+
+usage(){
+  echo "Usage $0 [-n <namespace> ] <group-name>"
+}
+
+#Error if we try to set the same variable twice
+set_once(){
+  local var_name=$1
+  shift
+  if [ -z "${!var_name}" ]; then
+    export $var_name="${1}"
+  else
+    echo "Tried to set '$var_name' twice!"
+    usage
+    exit 1
+  fi
+
+}
+
+while getopts 'n:h' arg; do
+case $arg in
+  n) set_once NAMESPACE "$OPTARG" ;;
+  h) usage; exit 1 ;;
+  \?) echo "Unrecognized option '$OPTARG'"; usage; exit 1 ;;
+esac
+done
 
 if [ -z "$1" ]; then
-  echo "Usage kommandeer <group-name> [<namespace>]"
+  usage
   exit 1
 fi
+
+set -e
+set -u
+set -o pipefail
 
 GROUP="$1"
 ALL_PVS="$(kubectl get pv -l kommandeer/group=$GROUP -o json)"
@@ -21,7 +53,7 @@ VOLUME_NAME=$(printf '%s\n' "$FIRST_RELEASED_PV" | jq '.metadata.name')
 
 #Use slurp to read in both the PV from memory and the template
 PVC=$(printf '%s\n' "$FIRST_RELEASED_PV" | cat pvc-template.json /dev/stdin \
- | jq --slurp --arg namePrefix "$GROUP-" -f build-pvc.jq | kubectl ${2:+-n $2} create -f - -o yaml)
+ | jq --slurp --arg namePrefix "$GROUP-" -f build-pvc.jq | kubectl ${NAMESPACE:+-n $NAMESPACE} create -f - -o yaml)
 
 #Manually attach the targeted volume by setting claimRef directly
 PATCH=$(printf '%s\n' "$PVC" \
